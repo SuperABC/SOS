c787802cfbd9237cd24caa3c68dd0957f74c490c
SuperABC <1363028533@qq.com>
2017-10-31 09:53:37 +0800

    Update.

---
+++ desktop.h
@@ -0,0 +1,13 @@
+#ifndef SOS_DESKTOP
+#define SOS_DESKTOP
+#include "main.h"
+
+void desktopInit();
+int desktopKey(int key);
+
+void backGround();
+void startList();
+void clearStart();
+void changeSelect(int dir);
+
+#endif
