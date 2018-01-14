88d8b4b4f550e3b91e8e1aad4d61a42ee98e76f9
SG master <1363028533@qq.com>
2018-01-14 18:22:46 +0800

    Update.

--- include/zjunix/bootmm.h
+++ include/zjunix/bootmm.h
@@ -1,40 +1,34 @@
-#ifndef _ZJUNIX_BOOTMM_H
-#define _ZJUNIX_BOOTMM_H
+#ifndef _ZJUNIX_BOOTMEM_H
+#define _ZJUNIX_BOOTMEM_H
 
-extern unsigned char __end[];
-
-enum mm_usage { _MM_KERNEL, _MM_MMMAP, _MM_VGABUFF, _MM_PDTABLE, _MM_PTABLE, _MM_DYNAMIC, _MM_RESERVED, _MM_COUNT };
-
-// record every part of mm's information
-struct bootmm_info {
-    unsigned int start;
-    unsigned int end;
-    unsigned int type;
-};
-// typedef struct bootmm_info * p_mminfo;
-
-// 4K per page
 #define PAGE_SHIFT 12
+//4 * 1028 4KB
 #define PAGE_FREE 0x00
 #define PAGE_USED 0xff
-
 #define PAGE_ALIGN (~((1 << PAGE_SHIFT) - 1))
-
 #define MAX_INFO 10
 
-struct bootmm {
-    unsigned int phymm;    // the actual physical memory
-    unsigned int max_pfn;  // record the max page number
-    unsigned char* s_map;  // map begin place
-    unsigned char* e_map;
-    unsigned int last_alloc_end;
-    unsigned int cnt_infos;  // get number of infos stored in bootmm now
-    struct bootmm_info info[MAX_INFO];
-};
-// typedef struct bootmm * p_bootmm;
-
+extern unsigned char __end[];
 extern unsigned int firstusercode_start;
 extern unsigned int firstusercode_len;
+enum mm_usage {_MM_KERNEL, _MM_MMMAP, _MM_VGABUFF, _MM_PDTABLE, _MM_PTABLE, _MM_DYNAMIC, _MM_RESERVED, _MM_COUNT};
+
+struct bootmm_info
+{
+	unsigned int start;
+	unsigned int end;
+	unsigned int type;
+};
+
+struct bootmm {
+    unsigned int phymm;
+    unsigned int max_pfn;
+    unsigned char* s_map;
+    unsigned char* e_map;
+    unsigned int last_alloc_end;
+    unsigned int cnt_infos;
+    struct bootmm_info info[MAX_INFO];
+};
 
 extern struct bootmm bmm;
 
